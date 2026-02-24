f2be75939c0587ad8eb9098fe1d88c1acc44ca8f
Md. Meraj Hossain <merajhossain028@gmail.com>
2026-02-24 01:58:48 +0000

    feat: created safari window

--- src/constants/index.js
+++ src/constants/index.js
@@ -74,28 +74,21 @@ const dockApps = [
   },
 ];
 
-const blogPosts = [
+const socialMedia = [
   {
     id: 1,
-    date: "Sep 2, 2025",
+    date: "",
     title:
-      "TypeScript Explained: What It Is, Why It Matters, and How to Master It",
-    image: "/images/blog1.png",
-    link: "https://jsmastery.com/blog/typescript-explained-what-it-is-why-it-matters-and-how-to-master-it",
+      "LinkedIn",
+    image: "/images/linkedin.png",
+    link: "https://www.linkedin.com/in/md-meraj-hossain/",
   },
   {
     id: 2,
-    date: "Aug 28, 2025",
-    title: "The Ultimate Guide to Mastering Three.js for 3D Development",
-    image: "/images/blog2.png",
-    link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-three-js-for-3d-development",
-  },
-  {
-    id: 3,
-    date: "Aug 15, 2025",
-    title: "The Ultimate Guide to Mastering GSAP Animations",
-    image: "/images/blog3.png",
-    link: "https://jsmastery.com/blog/the-ultimate-guide-to-mastering-gsap-animations",
+    date: "",
+    title: "Github",
+    image: "/images/github.png",
+    link: "https://github.com/merajhossain028",
   },
 ];
 
@@ -217,7 +210,7 @@ const gallery = [
 ];
 
 export {
-    blogPosts,
+    socialMedia,
     dockApps,
     gallery,
     navIcons,
--- src/windows/index.js
+++ src/windows/index.js
@@ -1,4 +1,5 @@
 import Terminal from "#windows/Terminal.jsx";
+import Safari from "#windows/Safari.jsx";
 
-export { Terminal };
+export { Terminal, Safari };
 
